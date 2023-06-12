b81a825b6ac11acd242638eab7c651156ac1d1ce
Dace Ķelpe-Bērtiņa <kelpe.bertina@gmail.com>
2023-06-12 17:53:39 +0300

    Shorter version

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -15,33 +15,15 @@ describe("DemoQA-selectable", () => {
     GridPage.row2.contains("Six").click();
     GridPage.row3.contains("Eight").click();
     //Validate that Two, Four, Six, Eight are highlighted
-    GridPage.row1
-      .contains("Two")
-      .should("have.class", "list-group-item active list-group-item-action");
-    GridPage.row2
-      .contains("Four")
-      .should("have.class", "list-group-item active list-group-item-action");
-    GridPage.row2
-      .contains("Six")
-      .should("have.class", "list-group-item active list-group-item-action");
-    GridPage.row3
-      .contains("Eight")
-      .should("have.class", "list-group-item active list-group-item-action");
+    GridPage.row1.contains("Two").should("have.class", "active");
+    GridPage.row2.contains("Four").should("have.class", "active");
+    GridPage.row2.contains("Six").should("have.class", "active");
+    GridPage.row3.contains("Eight").should("have.class", "active");
     //Validate that One, Three, Five, Seven, Nine are not highlighted
-    GridPage.row1
-      .contains("One")
-      .should("have.class", "list-group-item list-group-item-action");
-    GridPage.row1
-      .contains("Three")
-      .should("have.class", "list-group-item list-group-item-action");
-    GridPage.row2
-      .contains("Five")
-      .should("have.class", "list-group-item list-group-item-action");
-    GridPage.row3
-      .contains("Seven")
-      .should("have.class", "list-group-item list-group-item-action");
-    GridPage.row3
-      .contains("Nine")
-      .should("have.class", "list-group-item list-group-item-action");
+    GridPage.row1.contains("One").should("not.have.class", "active");
+    GridPage.row1.contains("Three").should("not.have.class", "active");
+    GridPage.row2.contains("Five").should("not.have.class", "active");
+    GridPage.row3.contains("Seven").should("not.have.class", "active");
+    GridPage.row3.contains("Nine").should("not.have.class", "active");
   });
 });
